1ed7ab8417ede5d6e5ca6da981f7f06836ba9898
CodeChrisB <c.buchberger01@gmail.com>
2023-01-17 01:55:55 +0100

    Init

---
+++ data.js
@@ -0,0 +1,58 @@
+const GAMES = Object.freeze([
+    //Pack 1
+    {name:"Drawful",pack:"1",min:3,max:8},
+    {name:"Fibbage XL",pack:"1",min:2,max:8},
+    {name:"Lie Swatter",pack:"1",min:1,max:100},
+    {name:"Word Spud",pack:"1",min:2,max:8},
+    {name:"You dont know Jack",pack:"1",min:1,max:4},
+    //Pack 2
+    {name:"Bidiots ",pack:"2",min:3,max:6},
+    {name:"Bomb Corp",pack:"2",min:1,max:4},
+    {name:"Earwax ",pack:"2",min:3,max:8},
+    {name:"Fibbage",pack:"2",min:2,max:8},
+    {name:"Quiplash XL",pack:"2",min:3,max:8},
+    //Pack 3
+    {name:"Quiplash 2 ",pack:"3",min:3,max:8},
+    {name:"Trivia Murder Party",pack:"3",min:1,max:8},
+    {name:"Guesspionage  ",pack:"3",min:2,max:8},
+    {name:"Tee K.O.",pack:"3",min:3,max:8},
+    {name:"Fakin' It",pack:"3",min:3,max:6},
+    //Pack 4
+    {name:"Bracketeering",pack:"4",min:3,max:16},
+    {name:"Civic Doodle",pack:"4",min:3,max:8},
+    {name:"Fibbage 3",pack:"4",min:2,max:8},
+    {name:"Monster Seeking Monster",pack:"4",min:3,max:7},
+    {name:"Survive the Internet",pack:"4",min:3,max:8},
+    //Pack 5
+    {name:"YOU DON’T KNOW JACK",pack:"5",min:1,max:8},
+    {name:"What-if",pack:"5",min:3,max:8},
+    {name:"Mad Verse City",pack:"5",min:3,max:8},
+    {name:"Patently Stupid",pack:"5",min:3,max:8},
+    {name:"Zeeple Doom",pack:"5",min:1,max:6},
+    //Pack 6
+    {name:"Dictionarium",pack:"6",min:3,max:8},
+    {name:"Joke Boat",pack:"6",min:3,max:8},
+    {name:"Push The Button",pack:"6",min:4,max:10},
+    {name:"Role Models",pack:"6",min:3,max:6},
+    {name:"Trivia Murder Party 2",pack:"6",min:1,max:8},
+    //Pack 7
+    {name:"Blather 'Round",pack:"7",min:2,max:6},
+    {name:"Champ’d Up",pack:"7",min:3,max:8},
+    {name:"Quiplash 3",pack:"7",min:3,max:8},
+    {name:"Talking Points",pack:"7",min:3,max:8},
+    {name:"The Devils and the Details",pack:"7",min:3,max:8},
+    //Pack 8
+    {name:"Drawful Animate",pack:"8",min:3,max:10},
+    {name:"The Wheel of Enormous Proportions",pack:"8",min:2,max:8},
+    {name:"Job Job",pack:"8",min:3,max:10},
+    {name:"The Poll Mine",pack:"8",min:2,max:10},
+    {name:"Weapons Drawn",pack:"8",min:4,max:8},
+    //Pack 9
+    {name:"Fibbage 4",pack:"9",min:2,max:8},
+    {name:"Quixort",pack:"8",min:1,max:10},
+    {name:"Junktopia",pack:"8",min:3,max:8},
+    {name:"Nonsensory",pack:"8",min:3,max:8},
+    {name:"Roomerang",pack:"8",min:4,max:9},
+])
+
+const Header = Object.freeze({name:"Name",pack:"Pack",min:"Min",max:"Max"})
---
+++ script.js
@@ -0,0 +1,43 @@
+function getGameList() {
+    console.log('hello')
+    let html = GAMES
+    let nameFilter = document.getElementById("nameInput").value.toLowerCase()
+    //let packInput = document.getElementById("packInput").value
+    let packs = getPacks()
+    console.log('pack',packs)
+    let maxInput = document.getElementById("maxInput").value
+    let minInput = document.getElementById("minInput").value
+
+    html = html
+    .filter(x=>x.name.toLowerCase().includes(nameFilter)) //name
+    .filter(x=>getPacks().includes(x.pack)) //Pack
+    .filter(x=>x.max <= maxInput) //max
+    .filter(x=>!(x.min > minInput)) //min
+    html = html.map(x=>getGameRow(x)) 
+
+    html.unshift(getGameRow(Header))
+    document.getElementById("gamelist").innerHTML = html.join('')
+}
+
+function getPacks(){
+    return [
+        document.getElementById("jp1").checked ? "1" : null,
+        document.getElementById("jp2").checked ? "2" : null,
+        document.getElementById("jp3").checked ? "3" : null,
+        document.getElementById("jp4").checked ? "4" : null,
+        document.getElementById("jp5").checked ? "5" : null,
+        document.getElementById("jp6").checked ? "6" : null,
+        document.getElementById("jp7").checked ? "7" : null,
+        document.getElementById("jp8").checked ? "8" : null,
+        document.getElementById("jp9").checked ? "9" : null,
+    ].filter(x=>x)
+}
+
+function getGameRow(data){
+    return `<tr>
+        <td>${data.name}</td>
+        <td>${data.pack}</td>
+        <td>${data.min}</td>
+        <td>${data.max}</td>
+    </tr>`
+}
